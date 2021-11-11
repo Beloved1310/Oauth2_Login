76182fd99291fd837d86dc5bf24d6426b097c0d3
Adefisayo1310 <adefisayo@jamitfm.com>
2021-11-11 18:09:22 +0100

    feat: cleared comment

--- src/Component/RequestToken.jsx
+++ src/Component/RequestToken.jsx
@@ -1,63 +1,57 @@
-import React from 'react'
-import qs from 'qs'
-import axios from 'axios'
-import { useHistory } from 'react-router-dom'
-const client_id = "wprQYMZBqqx-dgszFUfQG"
-const redirect_uri = "http://localhost:3000/oauth-callback";
-
+import React from 'react';
+import qs from 'qs';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+const client_id = 'wprQYMZBqqx-dgszFUfQG';
+const redirect_uri = 'http://localhost:3000/oauth-callback';
 
 function RequestToken() {
-    const history = useHistory()
+    const history = useHistory();
     const requestToken = async () => {
         // const tokenEndpoint = new URL(
         //     "https://id-sandbox.cashtoken.africa/oauth/token"
         //   );
         //     console.log(tokenEndpoint)
         const currentLocation = new URL(window.location.href);
-        const authorizationCode = currentLocation.searchParams.get("code");
-        const stateFromLocation = currentLocation.searchParams.get("state");
-        const initialCodeVerifier = window.sessionStorage.getItem("code_verifier");
-        console.log(authorizationCode)
-        if (window.sessionStorage.getItem("state") !== stateFromLocation){
-            throw Error("Probable session hijacking attack!");
+        const authorizationCode = currentLocation.searchParams.get('code');
+        const stateFromLocation = currentLocation.searchParams.get('state');
+        const initialCodeVerifier = window.sessionStorage.getItem('code_verifier');
+        console.log(authorizationCode);
+        if (window.sessionStorage.getItem('state') !== stateFromLocation) {
+            throw Error('Probable session hijacking attack!');
         }
         const queryParams = {
             client_id,
-            grant_type: "authorization_code",
+            grant_type: 'authorization_code',
             state: stateFromLocation,
             code: authorizationCode,
             code_verifier: initialCodeVerifier,
-            redirect_uri
-          };
-          console.log(queryParams)
+            redirect_uri,
+        };
+        console.log(queryParams);
 
-         
-            var options = {
-                method: 'POST',
-                headers: {'content-type': 'application/x-www-form-urlencoded'},
-                data: qs.stringify(queryParams),
-                url: 'https://id-sandbox.cashtoken.africa/oauth/token?',
-              };
-              
-              console.log(options)
-              
-    
-              
-              const response = await axios(options)
-                console.log(response.data.access_token);
-                localStorage.setItem('session_cookie', JSON.stringify(response.data.access_token))
-           
-            history.push('/profile')
-            
-       
+        var options = {
+            method: 'POST',
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            data: qs.stringify(queryParams),
+            url: 'https://id-sandbox.cashtoken.africa/oauth/token?',
+        };
+
+        console.log(options);
+
+        const response = await axios(options);
+        console.log(response.data.access_token);
+        localStorage.setItem('session_cookie', JSON.stringify(response.data.access_token));
+
+        history.push('/profile');
     };
     return (
         <div>
-           <button className='btn clear-btn' onClick={requestToken}>
+            <button className='btn clear-btn' onClick={requestToken}>
                 TOKEN GENERATION
-            </button> 
+            </button>
         </div>
-    )
+    );
 }
 
-export default RequestToken
+export default RequestToken;
